74f3e2d0876ec8687e56f7b1dc6f10c9532086ee
Quyen Huynh <quyen.huynh@asnet.com.vn>
2025-08-13 17:35:30 +0700

    feat: config theme and impelement button component

---
+++ src/theme/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  colors: {
+    brand: {
+      primary: '#FFAC12',
+      secondary: '#979797',
+      accent: '#E4E4E4',
+      //   neutral: '#F3F4F6', // background/neutral
+    },
+  },
+};
---
+++ src/theme/typography.ts
@@ -0,0 +1,11 @@
+export const fontSize = {
+  fontSize: {
+    tiny: '10px',
+    base: '15px',
+    md: '17px',
+    '2md': '20px',
+    '3md': '42px',
+    lg: '50px',
+    '2lg': '80px',
+  },
+};
