b7cfb257276f2c08e176df0e344ce1b35b3e1e03
Quyen Huynh <quyen.huynh@asnet.com.vn>
2025-08-14 14:51:08 +0700

    feat: implement select component

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,8 +1,16 @@
 import Button from './Button';
 import { Icons } from './Icons';
 import PropertyCard from './PropertyCard';
+import Select from './Select';
 
 const Test = () => {
+  const locations = [
+    { value: '', label: 'Locations' },
+    { value: 'us', label: 'United States' },
+    { value: 'uk', label: 'United Kingdom' },
+    { value: 'ca', label: 'Canada' },
+  ];
+
   return (
     <>
       <Button rightIcon={<Icons.Path className="w-5 h-10 text-white ml-2" />}>
@@ -22,6 +30,14 @@ const Test = () => {
         beds={4}
         baths={2}
         sun={2}
+      />
+
+      <Select
+        options={locations}
+        name="location"
+        className="w-267"
+        // value={selectedValue}
+        // onChange={handleChange}
       />
     </>
   );
