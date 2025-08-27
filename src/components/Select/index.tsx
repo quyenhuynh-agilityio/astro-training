14792175334e3d2dd7e0e962ddb6ab6a3ccfa846
Quyen Huynh <quyen.huynh@asnet.com.vn>
2025-08-27 16:53:37 +0700

    feat: correct type of title

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -41,8 +41,8 @@ const Select: React.FC<SelectProps> = ({
         onChange={onChange}
         className={`text-md pl-10 py-12 font-bold focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${className}`}
       >
-        {options?.map((option) => (
-          <option key={option.value} value={option.value}>
+        {options?.map((option, index) => (
+          <option key={`${option.value}-${index}`} value={option.value}>
             {option.label}
           </option>
         ))}
